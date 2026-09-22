3850b5d65c6178b4e8947a67e3d629ae123c60e7
BILGISOLUTIONS <ozan@zantrades.com>
2026-09-22 03:11:15 -0500

    Process PBR draft maps off the main browser thread

---
+++ apps/configurator/src/material-processor.worker.ts
@@ -0,0 +1,74 @@
+import {
+  averageRgb,
+  deriveBaseColor,
+  deriveNormalMap,
+  deriveRoughnessMap,
+  rgbToHex,
+  type BaseColorOptions,
+  type NormalOptions,
+  type PixelImage,
+  type RoughnessOptions,
+} from '@sls/material-processor'
+
+interface ProcessRequest {
+  type: 'process'
+  jobId: string
+  frames: {
+    crossPolarized: PixelImage
+    parallel: PixelImage
+    north: PixelImage
+    east: PixelImage
+    south: PixelImage
+    west: PixelImage
+  }
+  options: {
+    baseColor: BaseColorOptions
+    normal: NormalOptions
+    roughness: RoughnessOptions
+  }
+}
+
+self.onmessage = (event: MessageEvent<ProcessRequest>) => {
+  const message = event.data
+  if (message.type !== 'process') return
+
+  try {
+    const baseColor = deriveBaseColor(message.frames.crossPolarized, message.options.baseColor)
+    const roughness = deriveRoughnessMap(
+      message.frames.crossPolarized,
+      message.frames.parallel,
+      message.options.roughness,
+    )
+    const normal = deriveNormalMap(
+      message.frames.north,
+      message.frames.east,
+      message.frames.south,
+      message.frames.west,
+      message.options.normal,
+    )
+
+    const previewColor = rgbToHex(averageRgb(baseColor))
+
+    self.postMessage(
+      {
+        type: 'complete',
+        jobId: message.jobId,
+        maps: { baseColor, roughness, normal },
+        previewColor,
+      },
+      {
+        transfer: [
+          baseColor.data.buffer,
+          roughness.data.buffer,
+          normal.data.buffer,
+        ],
+      },
+    )
+  } catch (error) {
+    self.postMessage({
+      type: 'error',
+      jobId: message.jobId,
+      message: error instanceof Error ? error.message : 'Material processing failed.',
+    })
+  }
+}
